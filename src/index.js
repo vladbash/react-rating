5ce55ec8c8113f038625c42d419dc10869eb9240
vladb95 <vladb951@gmail.com>
2017-09-28 02:41:40 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
+
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NextApp = require('./App').default
+      ReactDOM.render(
+        <NextApp />,
+        rootElement
+      )
+    })
+  }
+
 registerServiceWorker();
